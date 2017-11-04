f6c8b7a9365dec4d3ca4e87493b476e7a551e6a6
Rubanraj <rubanraj54@gmail.com>
2017-11-04 22:27:02 +0100

    created shell script to emualted docker compose file for many robots
    need tp include mongodb container for logging

---
+++ neo4jtut/enterprise/spin-up.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#initialization
+
+IMAGE_NAME="neo-robot:latest"
+DEFAULT_PORT_NEO4J=7474
+DEFAULT_PORT_NEO4J_BOLT=7687
+COORDINATION_INSTANCE_PORT=5001
+DATA_PORT=6001
+INITIAL_HOSTS=""
+
+DOCKER_FILE_NAME="docker-compose-neo-robots.yml"
+echo -n "" > $DOCKER_FILE_NAME
+echo -e "version: '3'\n" >> $DOCKER_FILE_NAME
+echo "services:" >> $DOCKER_FILE_NAME
+
+if [ $# -eq 0 ]
+  then
+    DEFAULT_NUMBER_OF_ROBOTS=2
+else
+    # if (($i < 11))
+    #   DEFAULT_NUMBER_OF_ROBOTS=$1
+    # fi
+fi
+
+for (( i = 1; i <= $DEFAULT_NUMBER_OF_ROBOTS; i++ )); do
+  INITIAL_HOSTS="$INITIAL_HOSTS instance$i:$COORDINATION_INSTANCE_PORT,"
+done
+
+for ((i=1;i<=$DEFAULT_NUMBER_OF_ROBOTS;i++));
+do
+  echo $DEFAULT_NUMBER_OF_ROBOTS >> $DOCKER_FILE_NAME
+  echo -e "  instance$i:" >> $DOCKER_FILE_NAME
+  echo -e "    image: $IMAGE_NAME" >> $DOCKER_FILE_NAME
+  echo -e "    ports:" >> $DOCKER_FILE_NAME
+  echo -e "      - $((DEFAULT_PORT_NEO4J+i)):$DEFAULT_PORT_NEO4J" >> $DOCKER_FILE_NAME
+  echo -e "      - $((DEFAULT_PORT_NEO4J_BOLT+i)):$DEFAULT_PORT_NEO4J_BOLT" >> $DOCKER_FILE_NAME
+  echo -e "    volumes:" >> $DOCKER_FILE_NAME
+  echo -e "      - $HOME/neo4j-enterprise/logs$i:/logs" >> $DOCKER_FILE_NAME
+  echo -e "      - $HOME/neo4j-enterprise/data$i:/data" >> $DOCKER_FILE_NAME
+  echo -e "    hostname: instance$i" >> $DOCKER_FILE_NAME
+  echo -e "    networks:" >> $DOCKER_FILE_NAME
+  echo -e "      - cluster" >> $DOCKER_FILE_NAME
+  echo -e "    environment:" >> $DOCKER_FILE_NAME
+  echo -e "      - NEO4J_dbms_mode=HA" >> $DOCKER_FILE_NAME
+  echo -e "      - NEO4J_ha_serverId=$i" >> $DOCKER_FILE_NAME
+  echo -e "      - NEO4J_ha_host_coordination=instance$i:$COORDINATION_INSTANCE_PORT" >> $DOCKER_FILE_NAME
+  echo -e "      - NEO4J_ha_host_data=instance$i:$DATA_PORT" >> $DOCKER_FILE_NAME
+  echo -e "      - NEO4J_ha_initialHosts=$INITIAL_HOSTS" >> $DOCKER_FILE_NAME
+  if (( $i > 1 )); then
+    echo -e "    depends_on:" >> $DOCKER_FILE_NAME
+    echo -e "      - instance$((i-1))" >> $DOCKER_FILE_NAME
+  fi
+done
+echo "networks:" >> $DOCKER_FILE_NAME
+echo "  cluster:" >> $DOCKER_FILE_NAME
+echo "    driver: bridge" >> $DOCKER_FILE_NAME
+
+#docker-compose -f $DOCKER_FILE_NAME up
---
+++ robots/robot_neo.py
@@ -0,0 +1,8 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+print 'i am a robot one'
+
